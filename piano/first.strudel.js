4cf76d50bb6a25866e6a35f88d3ba3e612e55a10
admin <nnamdichibuzor777@gmail.com>
2026-06-13 14:03:34 -0700

    second piano melody

--- piano/first.strudel.js
+++ piano/first.strudel.js
@@ -1,9 +1,8 @@
 setcpm(120/4)
 
 stack(
-  sound("bd hh rim bd"),
 
-  n("0 2 4 <[6 8] [7 9]>")
+  n("0 2 4 <[6, 8] [7 9]>")
     .scale("C:minor")
     .sound("piano")
     .release(2)
---
+++ piano/second.strudel.js
@@ -0,0 +1,13 @@
+setcpm(120/4)
+
+stack(
+
+  n("0 2 4 <[6 8] [7 9] [9 10] [9 11]>")
+    .scale("A2:minor")
+    .sound("piano"),
+  
+  n("0 2 4 <6 7 9 11> 10 9 7 4")
+    .scale("A1:minor")
+    .sound("piano"),
+
+)
